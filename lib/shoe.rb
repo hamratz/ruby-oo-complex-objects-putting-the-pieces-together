94cdc71e96375b652b8922df7d5ced95a5d3e698
Marty Warfel <marthawarfel@gmail.com>
2020-12-09 16:24:15 -0600

    commit tree a1659c0fddf4fa4b61ddb26145dbf54c3a0d31fb

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,27 @@
 # Make your shoe class here!
+class Shoe 
+    attr_accessor :color, :size, :material, :condition
+    attr_reader :brand
+
+
+def initialize(brand)
+    @brand = brand
+end
+
+def cobble
+    @condition = "new" #is the condition new? if so puts "Your shoe is as good as new!"
+    puts "Your shoe is as good as new!"
+end
+end
+
+
+=begin
+initialize brand
+
+brand
+color
+size
+material
+repaired shoe
+new shoe condition
+=end
